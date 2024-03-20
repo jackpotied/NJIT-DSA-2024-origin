3854d1ec3fb2222e55957e3b0c2881a8495a8024
jackpotied <a2535562225@outlook.com>
2024-03-20 19:48:25 +0800

    ok

---
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -0,0 +1,35 @@
+package oy.tol.tra;
+
+public class Algorithms {
+    public static <T> void swap(T[]array,int a,int b){
+        T temp;
+       temp=array[a];
+       array[a]=array[b];
+       array[b]=temp;
+    }
+    public static <T extends Comparable<T>> void sort(T [] array) {
+        for(int i=0;i<array.length-1;i++){
+            for(int j=0;j< array.length-i-1;j++){
+                if(array[j].compareTo(array[j+1])>0){
+                    swap(array,j,j+1);
+                }
+            }
+        }
+
+    }
+
+
+
+    public static <T> void reverse(T [] array) {
+            for(int i=0;i<=array.length/2-1;i++){
+                swap(array,i, array.length-i-1);
+            }
+    }
+    public static <T extends Comparable<T>> int binarySearch(T aValue, T [] fromArray, int fromIndex, int toIndex) {
+        // TODO: you will implement this in step 2 below.
+        return 0;
+    }
+
+
+
+}
